73d888d8a105f685184b41d34d32fbcfb18c2bf7
nIKolaiamIRI557 <napkin-taunts.0m@icloud.com>
2025-09-13 12:11:23 +0100

    Setup 交互式图表生成器 using ENTITYFRAMEWORK

---
+++ InteractiveChartGenerator_0913_1911_jzn.cs
@@ -0,0 +1,17 @@
+// 代码生成时间: 2025-09-13 19:11:22
+ * InteractiveChartGenerator.cs
+ *
+ * This class provides functionality to generate interactive charts using Entity Framework.
+ * It demonstrates best practices for C# programming, including clear structure,
+ * error handling, documentation, and maintainability.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace InteractiveChartGenerator
+{
+    public class ChartContext : DbContext
