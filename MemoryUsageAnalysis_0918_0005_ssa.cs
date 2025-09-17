32953825b8bcd4f1acc2b3ca68699fc20cddb48a
nIKolaiamIRI557 <napkin-taunts.0m@icloud.com>
2025-09-17 17:05:54 +0100

    Develop CSHARP 内存使用情况分析

---
+++ MemoryUsageAnalysis_0918_0005_ssa.cs
@@ -0,0 +1,16 @@
+// 代码生成时间: 2025-09-18 00:05:53
+// <copyright file="MemoryUsageAnalysis.cs" company="Your Company">
+//   Copyright (C) 2023 Your Company. All rights reserved.
+// </copyright>
+
+using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Provides functionality for analyzing memory usage.
+/// </summary>
+public class MemoryUsageAnalysis
+{
+    /// <summary>
+    /// The <see cref="DbContextOptionsBuilder
